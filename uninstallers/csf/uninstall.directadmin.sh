4c6b9e4ae2ab633597caae1590cecbf5eb38dd43
Daniel Nowakowski <asti.nowak@gmail.com>
2025-09-15 02:56:37 +0000

    Uninstallers: make DirectAdmin script a true uninstaller; robustly remove qhtl units, cron, binaries, DA plugin, and configs

--- uninstallers/csf/uninstall.directadmin.sh
+++ uninstallers/csf/uninstall.directadmin.sh
@@ -1,61 +1,66 @@
 #!/bin/sh
-echo "Uninstalling csf and lfd..."
+echo "Uninstalling qhtlfirewall and qhtlwaterfall..."
 echo
 
-sed -i 's/lfd=ON/lfd=OFF/' /usr/local/directadmin/data/admin/services.status
+# Disable DA service monitor entry if present
+sed -i 's/lfd=ON/lfd=OFF/' /usr/local/directadmin/data/admin/services.status 2>/dev/null
 
-/usr/sbin/csf -f
+# Flush firewall rules before removing binaries
+/usr/sbin/qhtlfirewall -f 2>/dev/null || true
 
-if test `cat /proc/1/comm` = "systemd"
+if test "$(cat /proc/1/comm)" = "systemd"
 then
-    systemctl disable qhtlfirewall.service
-    systemctl disable qhtlwaterfall.service
-    systemctl stop qhtlwaterfall.service
-    systemctl stop qhtlfirewall.service
+    systemctl disable qhtlfirewall.service 2>/dev/null || true
+    systemctl disable qhtlwaterfall.service 2>/dev/null || true
+    systemctl stop qhtlwaterfall.service 2>/dev/null || true
+    systemctl stop qhtlfirewall.service 2>/dev/null || true
 
     rm -fv /usr/lib/systemd/system/qhtlfirewall.service
     rm -fv /usr/lib/systemd/system/qhtlwaterfall.service
-    systemctl daemon-reload
+    systemctl daemon-reload 2>/dev/null || true
 else
     if [ -f /etc/redhat-release ]; then
-        /sbin/chkconfig csf off
-        /sbin/chkconfig lfd off
-        /sbin/chkconfig csf --del
-        /sbin/chkconfig lfd --del
+        /sbin/chkconfig qhtlfirewall off 2>/dev/null || true
+        /sbin/chkconfig qhtlwaterfall off 2>/dev/null || true
+        /sbin/chkconfig qhtlfirewall --del 2>/dev/null || true
+        /sbin/chkconfig qhtlwaterfall --del 2>/dev/null || true
     elif [ -f /etc/debian_version ] || [ -f /etc/lsb-release ]; then
-        update-rc.d -f lfd remove
-        update-rc.d -f csf remove
+        update-rc.d -f qhtlwaterfall remove 2>/dev/null || true
+        update-rc.d -f qhtlfirewall remove 2>/dev/null || true
     elif [ -f /etc/gentoo-release ]; then
-        rc-update del lfd default
-        rc-update del csf default
-    elif [ -f /etc/slackware-version ]; then
-        rm -vf /etc/rc.d/rc3.d/S80csf
-        rm -vf /etc/rc.d/rc4.d/S80csf
-        rm -vf /etc/rc.d/rc5.d/S80csf
-        rm -vf /etc/rc.d/rc3.d/S85lfd
-        rm -vf /etc/rc.d/rc4.d/S85lfd
-        rm -vf /etc/rc.d/rc5.d/S85lfd
+        rc-update del qhtlwaterfall default 2>/dev/null || true
+        rc-update del qhtlfirewall default 2>/dev/null || true
+        rm -fv /etc/init.d/qhtlfirewall
+        rm -fv /etc/init.d/qhtlwaterfall
+        rm -vf /etc/rc.d/rc3.d/S80qhtlfirewall
+        rm -vf /etc/rc.d/rc4.d/S80qhtlfirewall
+        rm -vf /etc/rc.d/rc5.d/S80qhtlfirewall
+        rm -vf /etc/rc.d/rc3.d/S85qhtlwaterfall
+        rm -vf /etc/rc.d/rc4.d/S85qhtlwaterfall
+        rm -vf /etc/rc.d/rc5.d/S85qhtlwaterfall
     else
-        /sbin/chkconfig csf off
-        /sbin/chkconfig lfd off
-        /sbin/chkconfig csf --del
-        /sbin/chkconfig lfd --del
+        /sbin/chkconfig qhtlfirewall off 2>/dev/null || true
+        /sbin/chkconfig qhtlwaterfall off 2>/dev/null || true
+        /sbin/chkconfig qhtlfirewall --del 2>/dev/null || true
+        /sbin/chkconfig qhtlwaterfall --del 2>/dev/null || true
     fi
-    rm -fv /etc/init.d/csf
-    rm -fv /etc/init.d/lfd
+    # Clean up any legacy init names if they happen to exist
+    rm -fv /etc/init.d/qhtlfirewall /etc/init.d/qhtlwaterfall /etc/init.d/csf /etc/init.d/lfd
 fi
 
-rm -fv /etc/chkserv.d/lfd
-rm -fv /usr/sbin/csf
-rm -fv /usr/sbin/lfd
-rm -fv /etc/cron.d/csf_update
-rm -fv /etc/cron.d/lfd-cron
-rm -fv /etc/cron.d/csf-cron
-rm -Rfv /usr/local/directadmin/plugins/csf
-rm -fv /etc/logrotate.d/lfd
-rm -fv /usr/local/man/man1/csf.man.1
+# Remove scheduled tasks and logrotate
+rm -fv /etc/cron.d/qhtlwaterfall-cron /etc/cron.d/qhtlfirewall-cron /etc/cron.d/csf_update
+rm -fv /etc/logrotate.d/qhtlwaterfall
 
-rm -Rfv /etc/csf /usr/local/csf /var/lib/csf
+# Remove binaries and manpage
+rm -fv /usr/sbin/qhtlfirewall /usr/sbin/qhtlwaterfall
+rm -fv /usr/local/man/man1/csf.1
+
+# Remove DirectAdmin plugin
+rm -Rfv /usr/local/directadmin/plugins/csf
+
+# Remove configuration and data directories
+rm -Rfv /etc/qhtlfirewall /usr/local/qhtlfirewall /var/lib/qhtlfirewall
 
 echo
 echo "...Done"
